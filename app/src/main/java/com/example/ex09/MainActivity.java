3874a77833c5d3dfe186df172ed9c19767b13ea9
gs7073 <gs7073@bs.amalnet.k12.il>
2025-04-18 21:06:53 +0300

    java

--- app/src/main/java/com/example/ex09/MainActivity.java
+++ app/src/main/java/com/example/ex09/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex09;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,37 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText txt;
+    Button btn;
+    WebView web;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        txt = findViewById(R.id.txt);
+        btn = findViewById(R.id.btn);
+        web = findViewById(R.id.web);
+
+        web.setWebViewClient(new MyWebViewClient());
+
+        web.getSettings().setJavaScriptEnabled(true);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                web.loadUrl("https://www.google.com");
+
+            }
         });
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
